30e3cd6966b7fdeac86f96d0402c98a906832bac
Måns Kronkvist Brusbäck <manskron@gmail.com>
2020-05-17 22:09:09 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tinymd
+
+A markdown compiler written in rust, following [this tutorial](https://jesselawson.org/rust/getting-started-with-rust-by-building-a-tiny-markdown-compiler/)
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+fn parse_markdown_file() {
+
+}
+
+fn print_short_banner() {
+    println!("{}", get_title());
+}
+
+fn print_long_banner() {
+    let pkg_authors = String::from(env!("CARGO_PKG_AUTHORS"));
+    let pkg_homepage = String::from(env!("CARGO_PKG_HOMEPAGE"));
+
+    print_short_banner();
+    println!("Written by: {}", pkg_authors);
+    println!("Homepage: {}\n", pkg_homepage);
+}
+
+fn get_title() -> String {
+    let pkg_name = String::from(env!("CARGO_PKG_NAME"));
+    let pkg_version = String::from(env!("CARGO_PKG_VERSION"));
+    let pkg_description = String::from(env!("CARGO_PKG_DESCRIPTION"));
+
+    format!("{} (v{}), {}", pkg_name, pkg_version, pkg_description)
+}
+
+fn usage() {
+    print_long_banner();
+}
+
+fn main() {
+  usage();
+}
